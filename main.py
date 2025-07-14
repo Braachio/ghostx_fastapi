6099b06f096b2efa3321ae6a8345b24bfed34f06
Braachio <166890773+Braachio@users.noreply.github.com>
2025-07-14 10:28:50 +0900

    main.py 수정

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
 
 # API 모듈 import
 from api.analyze import router as analyze_router
@@ -19,3 +21,9 @@ app.add_middleware(
 # 라우터 등록
 app.include_router(analyze_router, prefix="/api")
 app.include_router(motec_router, prefix="/api")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.environ.get("PORT", 8000))  # Render는 기본적으로 10000
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
