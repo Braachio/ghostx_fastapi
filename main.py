ea7f4ac260743cbc60077fa3131975fccedca5a7
Braachio <166890773+Braachio@users.noreply.github.com>
2025-07-21 19:00:34 +0900

    테스트용 배포_점검

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://ghostx.site",           # 🔒 운영 도메인
-        "http://localhost:3000"          # 💻 개발용 도메인
+        "http://localhost:8000"          # 💻 개발용 도메인
     ],
     allow_credentials=True,
     allow_methods=["*"],
